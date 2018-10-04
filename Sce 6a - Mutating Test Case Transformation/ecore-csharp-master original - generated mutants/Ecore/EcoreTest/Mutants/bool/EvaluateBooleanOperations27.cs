eaa60e1620e11a92204e75e2ac470d04caa0c46e
Achyuth Nagaraj B <nagaraj@mail.uni-paderborn.de>
2018-10-04 17:44:26 +0200

    Revert "Revert "Scenario 5""
    This reverts commit 24b31226a1e06b5b73741ea4a4c80fb9248e5d77.

--- Sce 6a - Mutating Test Case Transformation/ecore-csharp-master original - generated mutants/Ecore/EcoreTest/Mutants/bool/EvaluateBooleanOperations27.cs
+++ Sce 6a - Mutating Test Case Transformation/ecore-csharp-master original - generated mutants/Ecore/EcoreTest/Mutants/bool/EvaluateBooleanOperations27.cs
@@ -6,416 +6,319 @@
 //{
 
 //    [TestClass]
-//    public class EvaluateNumericOperationsTest4
+//    public class EvaluateBooleanOperationsTest4
 //    {
 
 //        private TestOCL ocl = new TestOCL();
 
 //        [TestMethod]
-//        public void testNumber_1()
-//        {
-//            // Arrangement
-//            var expectedResult = 0;
-//            // Action
-//            var actualResult = 0;
-//            // Assertion 
-//            ocl.assertQueryEquals(expectedResult, actualResult);
-//        }
-
-//        [TestMethod]
-//        public void testNumber_2()
-//        {
-//            // Arrangement
-//            var expectedResult = 3;
-//            // Action
-//            var actualResult = 3;
-//            // Assertion 
-//            ocl.assertQueryEquals(expectedResult, actualResult);
-//        }
-
-//        [TestMethod]
-//        public void testNumber_3()
-//        {
-//            // Arrangement
-//            var expectedResult = 3.1;
-//            // Action
-//            var actualResult = 3.1;
-//            // Assertion 
-//            ocl.assertQueryEquals(expectedResult, actualResult);
-//        }
-
-//        [TestMethod]
-//        public void testNumberAbs_1()
-//        {
-//            // Arrangement
-//            var expectedResult = 3;
-//            // Action
-//            var actualResult = Math.Abs(3);
-//            // Assertion 
-//            ocl.assertQueryEquals(expectedResult, actualResult);
-//        }
-
-//        [TestMethod]
-//        public void testNumberAbs_2()
-//        {
-//            // Arrangement
-//            var expectedResult = 2147483647;
-//            // Action
-//            var actualResult = Math.Abs(2147483647);
-//            // Assertion 
-//            ocl.assertQueryEquals(expectedResult, actualResult);
-//        }
-
-//        [TestMethod]
-//        public void testNumberDiv_1()
-//        {
-//            // Arrangement
-//            var expectedResult = 1;
-//            // Action
-//            var actualResult = 3 / 2;
-//            // Assertion 
-//            ocl.assertQueryEquals(expectedResult, actualResult);
-//        }
-
-//        [TestMethod]
-//        public void testNumberDiv_2()
-//        {
-//            // Arrangement
-//            var expectedResult = 2;
-//            // Action
-//            var actualResult = 5 / 2;
-//            // Assertion 
-//            ocl.assertQueryEquals(expectedResult, actualResult);
-//        }
-
-//        [TestMethod]
-//        public void testNumberEqual_1()
+//        public void testBoolean_1()
 //        {
 //            // Arrangement
 //            /* no arrangement part since it's a single paramteter assertion */
 //            // Action
-//            var actualResult = 4.Equals(5);
+//            var actualResult = false;
 //            // Assertion 
 //            ocl.assertQueryFalse(actualResult);
 //        }
 
 //        [TestMethod]
-//        public void testNumberEqual_2()
+//        public void testBoolean_2()
 //        {
 //            // Arrangement
 //            /* no arrangement part since it's a single paramteter assertion */
 //            // Action
-//            var actualResult = 1.Equals(4.0);
-//            // Assertion 
-//            ocl.assertQueryFalse(actualResult);
-//        }
-
-//        [TestMethod]
-//        public void testNumberEqual_3()
-//        {
-//            // Arrangement
-//            /* no arrangement part since it's a single paramteter assertion */
-//            // Action
-//            var actualResult = 1.0.Equals(4);
-//            // Assertion 
-//            ocl.assertQueryFalse(actualResult);
-//        }
-
-//        [TestMethod]
-//        public void testNumberEqual_4()
-//        {
-//            // Arrangement
-//            /* no arrangement part since it's a single paramteter assertion */
-//            // Action
-//            var actualResult = 1.0.Equals(4.0);
-//            // Assertion 
-//            ocl.assertQueryFalse(actualResult);
-//        }
-
-//        [TestMethod]
-//        public void testNumberEqual_5()
-//        {
-//            // Arrangement
-//            /* no arrangement part since it's a single paramteter assertion */
-//            // Action
-//            var actualResult = 4.Equals(4);
+//            var actualResult = true;
 //            // Assertion 
 //            ocl.assertQueryTrue(actualResult);
 //        }
 
 //        [TestMethod]
-//        public void testNumberEqual_6()
+//        public void testBoolean_3()
 //        {
 //            // Arrangement
 //            /* no arrangement part since it's a single paramteter assertion */
 //            // Action
-//            var actualResult = 1.0.Equals(1);
-//            // Assertion 
-//            ocl.assertQueryTrue(actualResult);
-//        }
-
-//        [TestMethod]
-//        public void testNumberEqual_7()
-//        {
-//            // Arrangement
-//            /* no arrangement part since it's a single paramteter assertion */
-//            // Action
-//            var actualResult = 1.0.Equals(1.0);
-//            // Assertion 
-//            ocl.assertQueryTrue(actualResult);
-//        }
-
-//        [TestMethod]
-//        public void testNumberGreaterThan_1()
-//        {
-//            // Arrangement
-//            /* no arrangement part since it's a single paramteter assertion */
-//            // Action
-//            var actualResult = 3 > 2;
-//            // Assertion 
-//            ocl.assertQueryTrue(actualResult);
-//        }
-
-//        [TestMethod]
-//        public void testNumberGreaterThan_2()
-//        {
-//            // Arrangement
-//            /* no arrangement part since it's a single paramteter assertion */
-//            // Action
-//            var actualResult = 3.0 > 2.0;
-//            // Assertion 
-//            ocl.assertQueryTrue(actualResult);
-//        }
-
-//        [TestMethod]
-//        public void testNumberGreaterThan_3()
-//        {
-//            // Arrangement
-//            /* no arrangement part since it's a single paramteter assertion */
-//            // Action
-//            var actualResult = 3.0 > 3;
+//            var actualResult = ((Func<bool>)(() => { var b = false; return b; }))();
+//            ;
 //            // Assertion 
 //            ocl.assertQueryFalse(actualResult);
 //        }
 
 //        [TestMethod]
-//        public void testNumberGreaterThan_4()
+//        public void testBoolean_4()
 //        {
 //            // Arrangement
 //            /* no arrangement part since it's a single paramteter assertion */
 //            // Action
-//            var actualResult = 3 > 3.0;
-//            // Assertion 
-//            ocl.assertQueryFalse(actualResult);
-//        }
-
-//        [TestMethod]
-//        public void testNumberGreaterThanOrEqual_1()
-//        {
-//            // Arrangement
-//            /* no arrangement part since it's a single paramteter assertion */
-//            // Action
-//            var actualResult = 3 >= 2;
+//            var actualResult = ((Func<bool>)(() => { var b = true; return b; }))();
+//            ;
 //            // Assertion 
 //            ocl.assertQueryTrue(actualResult);
 //        }
 
 //        [TestMethod]
-//        public void testNumberGreaterThanOrEqual_2()
+//        public void testBooleanAnd_1()
 //        {
 //            // Arrangement
 //            /* no arrangement part since it's a single paramteter assertion */
 //            // Action
-//            var actualResult = 3.0 >= 3;
+//            var actualResult = false && false;
+//            // Assertion 
+//            ocl.assertQueryFalse(actualResult);
+//        }
+
+//        [TestMethod]
+//        public void testBooleanAnd_2()
+//        {
+//            // Arrangement
+//            /* no arrangement part since it's a single paramteter assertion */
+//            // Action
+//            var actualResult = false && true;
+//            // Assertion 
+//            ocl.assertQueryFalse(actualResult);
+//        }
+
+//        [TestMethod]
+//        public void testBooleanAnd_3()
+//        {
+//            // Arrangement
+//            /* no arrangement part since it's a single paramteter assertion */
+//            // Action
+//            var actualResult = true && false;
+//            // Assertion 
+//            ocl.assertQueryFalse(actualResult);
+//        }
+
+//        [TestMethod]
+//        public void testBooleanAnd_4()
+//        {
+//            // Arrangement
+//            /* no arrangement part since it's a single paramteter assertion */
+//            // Action
+//            var actualResult = true && true;
 //            // Assertion 
 //            ocl.assertQueryTrue(actualResult);
 //        }
 
 //        [TestMethod]
-//        public void testNumberLessThan_1()
+//        public void testBooleanEqual_1()
 //        {
 //            // Arrangement
 //            /* no arrangement part since it's a single paramteter assertion */
 //            // Action
-//            var actualResult = 3 < 2;
+//            var actualResult = true == false;
 //            // Assertion 
 //            ocl.assertQueryFalse(actualResult);
 //        }
 
 //        [TestMethod]
-//        public void testNumberLessThan_2()
+//        public void testBooleanEqual_2()
 //        {
 //            // Arrangement
 //            /* no arrangement part since it's a single paramteter assertion */
 //            // Action
-//            var actualResult = 3.0 < 2.0;
-//            // Assertion 
-//            ocl.assertQueryFalse(actualResult);
-//        }
-
-//        [TestMethod]
-//        public void testNumberLessThan_3()
-//        {
-//            // Arrangement
-//            /* no arrangement part since it's a single paramteter assertion */
-//            // Action
-//            var actualResult = 3 < 3.0;
-//            // Assertion 
-//            ocl.assertQueryFalse(actualResult);
-//        }
-
-//        [TestMethod]
-//        public void testNumberLessThanOrEqual_1()
-//        {
-//            // Arrangement
-//            /* no arrangement part since it's a single paramteter assertion */
-//            // Action
-//            var actualResult = 3 <= 2;
-//            // Assertion 
-//            ocl.assertQueryFalse(actualResult);
-//        }
-
-//        [TestMethod]
-//        public void testNumberMax_1()
-//        {
-//            // Arrangement
-//            var expectedResult = 3;
-//            // Action
-//            var actualResult = Math.Max(3, 2);
-//            // Assertion 
-//            ocl.assertQueryEquals(expectedResult, actualResult);
-//        }
-
-//        [TestMethod]
-//        public void testNumberMin_1()
-//        {
-//            // Arrangement
-//            var expectedResult = 2;
-//            // Action
-//            var actualResult = Math.Min(3, 2);
-//            // Assertion 
-//            ocl.assertQueryEquals(expectedResult, actualResult);
-//        }
-
-//        [TestMethod]
-//        public void testNumberMinus_1()
-//        {
-//            // Arrangement
-//            var expectedResult = 0;
-//            // Action
-//            var actualResult = 1 - 1;
-//            // Assertion 
-//            ocl.assertQueryEquals(expectedResult, actualResult);
-//        }
-
-//        [TestMethod]
-//        public void testNumberMod_1()
-//        {
-//            // Arrangement
-//            var expectedResult = 1;
-//            // Action
-//            var actualResult = 3 % 2;
-//            // Assertion 
-//            ocl.assertQueryEquals(expectedResult, actualResult);
-//        }
-
-//        [TestMethod]
-//        public void testNumberNotEqual_1()
-//        {
-//            // Arrangement
-//            /* no arrangement part since it's a single paramteter assertion */
-//            // Action
-//            var actualResult = 4 != 5;
+//            var actualResult = true == true;
 //            // Assertion 
 //            ocl.assertQueryTrue(actualResult);
 //        }
 
 //        [TestMethod]
-//        public void testNumberNotEqual_2()
+//        public void testBooleanEqual_3()
 //        {
 //            // Arrangement
 //            /* no arrangement part since it's a single paramteter assertion */
 //            // Action
-//            var actualResult = 1 != 4.0;
+//            var actualResult = false == false;
 //            // Assertion 
 //            ocl.assertQueryTrue(actualResult);
 //        }
 
 //        [TestMethod]
-//        public void testNumberNotEqual_3()
+//        public void testBooleanImplies_1()
 //        {
 //            // Arrangement
 //            /* no arrangement part since it's a single paramteter assertion */
 //            // Action
-//            var actualResult = 1.0 != 4;
+//            var actualResult = false || false;
 //            // Assertion 
 //            ocl.assertQueryTrue(actualResult);
 //        }
 
 //        [TestMethod]
-//        public void testNumberNotEqual_4()
+//        public void testBooleanImplies_2()
 //        {
 //            // Arrangement
 //            /* no arrangement part since it's a single paramteter assertion */
 //            // Action
-//            var actualResult = 1.0 != 4.0;
+//            var actualResult = false || true;
 //            // Assertion 
 //            ocl.assertQueryTrue(actualResult);
 //        }
 
 //        [TestMethod]
-//        public void testNumberNotEqual_5()
+//        public void testBooleanImplies_3()
 //        {
 //            // Arrangement
 //            /* no arrangement part since it's a single paramteter assertion */
 //            // Action
-//            var actualResult = 4 != 4;
+//            var actualResult = true || false;
 //            // Assertion 
 //            ocl.assertQueryFalse(actualResult);
 //        }
 
 //        [TestMethod]
-//        public void testNumberNotEqual_6()
+//        public void testBooleanImplies_4()
 //        {
 //            // Arrangement
 //            /* no arrangement part since it's a single paramteter assertion */
 //            // Action
-//            var actualResult = 1 != 1.0;
+//            var actualResult = true || true;
+//            // Assertion 
+//            ocl.assertQueryTrue(actualResult);
+//        }
+
+//        [TestMethod]
+//        public void testBooleanNot_1()
+//        {
+//            // Arrangement
+//            /* no arrangement part since it's a single paramteter assertion */
+//            // Action
+//            var actualResult = !false;
+//            // Assertion 
+//            ocl.assertQueryTrue(actualResult);
+//        }
+
+//        [TestMethod]
+//        public void testBooleanNot_2()
+//        {
+//            // Arrangement
+//            /* no arrangement part since it's a single paramteter assertion */
+//            // Action
+//            var actualResult = !true;
 //            // Assertion 
 //            ocl.assertQueryFalse(actualResult);
 //        }
 
 //        [TestMethod]
-//        public void testNumberNotEqual_7()
+//        public void testBooleanNotEqual_1()
 //        {
 //            // Arrangement
 //            /* no arrangement part since it's a single paramteter assertion */
 //            // Action
-//            var actualResult = 1.0 != 1;
+//            var actualResult = true != false;
+//            // Assertion 
+//            ocl.assertQueryTrue(actualResult);
+//        }
+
+//        [TestMethod]
+//        public void testBooleanNotEqual_2()
+//        {
+//            // Arrangement
+//            /* no arrangement part since it's a single paramteter assertion */
+//            // Action
+//            var actualResult = true != true;
 //            // Assertion 
 //            ocl.assertQueryFalse(actualResult);
 //        }
 
 //        [TestMethod]
-//        public void testNumberPlus_1()
+//        public void testBooleanNotEqual_3()
 //        {
 //            // Arrangement
-//            var expectedResult = 2;
+//            /* no arrangement part since it's a single paramteter assertion */
 //            // Action
-//            var actualResult = 1 + 1;
+//            var actualResult = false != false;
 //            // Assertion 
-//            ocl.assertQueryEquals(expectedResult, actualResult);
+//            ocl.assertQueryFalse(actualResult);
 //        }
 
 //        [TestMethod]
-//        public void testNumberTimes_1()
+//        public void testBooleanOr_1()
 //        {
 //            // Arrangement
-//            var expectedResult = 1;
+//            /* no arrangement part since it's a single paramteter assertion */
 //            // Action
-//            var actualResult = 1 % 1;
+//            var actualResult = false || false;
 //            // Assertion 
-//            ocl.assertQueryEquals(expectedResult, actualResult);
+//            ocl.assertQueryFalse(actualResult);
+//        }
+
+//        [TestMethod]
+//        public void testBooleanOr_2()
+//        {
+//            // Arrangement
+//            /* no arrangement part since it's a single paramteter assertion */
+//            // Action
+//            var actualResult = false || true;
+//            // Assertion 
+//            ocl.assertQueryTrue(actualResult);
+//        }
+
+//        [TestMethod]
+//        public void testBooleanOr_3()
+//        {
+//            // Arrangement
+//            /* no arrangement part since it's a single paramteter assertion */
+//            // Action
+//            var actualResult = true || false;
+//            // Assertion 
+//            ocl.assertQueryTrue(actualResult);
+//        }
+
+//        [TestMethod]
+//        public void testBooleanOr_4()
+//        {
+//            // Arrangement
+//            /* no arrangement part since it's a single paramteter assertion */
+//            // Action
+//            var actualResult = true || true;
+//            // Assertion 
+//            ocl.assertQueryTrue(actualResult);
+//        }
+
+//        [TestMethod]
+//        public void testBooleanXor_1()
+//        {
+//            // Arrangement
+//            /* no arrangement part since it's a single paramteter assertion */
+//            // Action
+//            var actualResult = false ^ false;
+//            // Assertion 
+//            ocl.assertQueryFalse(actualResult);
+//        }
+
+//        [TestMethod]
+//        public void testBooleanXor_2()
+//        {
+//            // Arrangement
+//            /* no arrangement part since it's a single paramteter assertion */
+//            // Action
+//            var actualResult = false ^ true;
+//            // Assertion 
+//            ocl.assertQueryTrue(actualResult);
+//        }
+
+//        [TestMethod]
+//        public void testBooleanXor_3()
+//        {
+//            // Arrangement
+//            /* no arrangement part since it's a single paramteter assertion */
+//            // Action
+//            var actualResult = true ^ false;
+//            // Assertion 
+//            ocl.assertQueryTrue(actualResult);
+//        }
+
+//        [TestMethod]
+//        public void testBooleanXor_4()
+//        {
+//            // Arrangement
+//            /* no arrangement part since it's a single paramteter assertion */
+//            // Action
+//            var actualResult = true ^ true;
+//            // Assertion 
+//            ocl.assertQueryFalse(actualResult);
 //        }
 
 
